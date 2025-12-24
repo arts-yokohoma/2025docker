fb3c3486982ec7b8c6eff7b23ddc01a52701f98e
aungmyintmyat08 <k247041@kccollege.ac.jp>
2025-12-24 12:04:16 +0900

    update index.php

--- team_2/app/customer/index.php
+++ team_2/app/customer/index.php
@@ -15,4 +15,18 @@
 </html>
 <?php
  echo "hello world";
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="logo.png" type="image/x-icon">
+    <title>ピザマック</title>
+ </head>
+ <body>
+    <h1>ピザマックへようこそ！</h1>
+    <p>こんにちは、ピザマックのお客様！</p>
+ </body>
+ </html>
  ?>

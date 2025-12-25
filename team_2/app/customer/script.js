03935842df7d272d7c26d12a0c8270e36d32e66d
mya812 <k247041@kccollege.ac.jp>
2025-12-25 21:01:04 +0900

    update cus fol

--- team_2/app/customer/script.js
+++ team_2/app/customer/script.js
@@ -1,5 +1,6 @@
 function toggleOrderSearch() {
     const orderSearch = document.getElementById("order-search");
+    console.log("Toggling order search visibility");
     if (orderSearch.style.display === "none" || orderSearch.style.display === "") {
         orderSearch.style.display = "block";
     } else {

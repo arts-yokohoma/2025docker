2fc89a0dde29c481c912d871482600cf6302e86a
arts-yokohoma <artsyokohoma@gmail.com>
2026-01-21 16:57:23 +0900

    Update test

--- team_2/app/admin/viewzip.php
+++ team_2/app/admin/viewzip.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Viwe AREA</title>
+    <title>Viwe test</title>
+    
 </head>
 <body>
     <h1>You can view avaliable area</h1>

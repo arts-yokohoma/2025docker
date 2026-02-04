9c59b887509d8e05afd2d71a678e9b4ead2095c5
MIN LIN HEIN OO <k247072@kccollege.ac.jp>
2026-02-04 09:43:29 +0900

    test

--- team_6/app/index.php
+++ team_6/app/index.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 echo "<h1>Team 6 - PHP + PostgreSQL</h1>";
 echo "<p>Hello from 00000000000000000sooo !</p>";
@@ -9,3 +10,16 @@ try {
     echo "<p>PostgreSQL Connection failed: " . $e->getMessage() . "</p>";
 }
 ?>
+=======
+<?php
+echo "<h1>Team 6 - PHP + PostgreSQL</h1>";
+echo "<p>Hello from 00000000000000ooo !</p>";
+
+try {
+    $pdo = new PDO("pgsql:host=team_6_db;dbname=team_6_db", "team_6", "team6pass");
+    echo "<p>PostgreSQL Connection successful!</p>";
+} catch (PDOException $e) {
+    echo "<p>PostgreSQL Connection failed: " . $e->getMessage() . "</p>";
+}
+?>
+>>>>>>> Stashed changes
